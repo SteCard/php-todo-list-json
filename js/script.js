c635e39393477def9f13987954743a05ac511126
SteCard <stefanocardillo14@gmail.com>
2023-07-06 19:27:08 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// RICHIAMO FUNZIONE CREATE_APP
+const { createApp } = Vue;
+
+// FUNZIONE CREATE_APP
+createApp({
+
+    data() {
+        return {
+        }
+    }
+}).mount('#app'); // RICHIAMO HTML PER VEDERE LA
